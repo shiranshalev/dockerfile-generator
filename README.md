04bf07364c4803f7d00872442a06805ffaace63a
Shiran Shalev <shirans@boltinc.com>
2022-01-16 19:26:10 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dockerfile-generator
 generates dockerfile with relevant references for .NET CORE applictaions
+
+run the powershell script from the relevant project's folder path.
+this script will generate a basic dockerfile with all of the project's dependencies.
+
+please notice you'll need to provide 3 parameters: \
+'mainProject': this will be the main project that the application is executing\
+'maindll': the main dll that the application should use \
+'maincsproj': the main csproj file that the application should use
+
---
+++ generate_dockerfile_with_dependencies.ps1
@@ -0,0 +1,73 @@
+
+Param(
+[Parameter(Mandatory = $True)]
+[string]$mainProject,
+[Parameter(Mandatory=$True)]
+[string]$dll,
+[Parameter(Mandatory=$True)]
+[string]$maincsproj
+)
+
+
+
+
+$csprojFiles = gci . -Filter *.csproj -Recurse
+$dockerFile = @"
+FROM mcr.microsoft.com/dotnet/aspnet:5.0-buster-slim AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+
+
+"@
+
+$copyLine = @"
+COPY ["{{filepath}}","{{fileDest}}"]
+
+"@
+
+$secondCopyLine = @"
+COPY ["{{fileDest}}","{{fileDest}}"]
+
+"@
+
+$spaceLine = @"
+
+
+"@
+
+$content = @"
+WORKDIR "/src/$($mainProject)"
+RUN dotnet build "$($maincsproj)" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "$($maincsproj)" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "$($dll)"]
+"@
+
+foreach($csproj in $csprojFiles){
+$currentPath = (pwd).Path
+$fullname = $csproj.fullname.replace($currentPath+'\','').replace('\','/')
+$dest = $fullname.Substring(0,$fullname.IndexOf('/'))
+$line = $copyLine -replace "{{filepath}}",$fullname -replace '{{fileDest}}',($dest+'/')
+$dockerFile+= $line 
+}
+
+$dockerFile+=$spaceLine
+
+foreach($csproj in $csprojFiles){
+$currentPath = (pwd).Path
+$fullname = $csproj.fullname.replace($currentPath+'\','').replace('\','/')
+$dest = $fullname.Substring(0,$fullname.IndexOf('/'))
+$secondLine = $secondCopyLine -replace '{{fileDest}}',($dest+'/')
+$dockerFile+=$secondLine
+}
+
+$dockerFile+=$spaceLine
+$dockerFile+= $content
